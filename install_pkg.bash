29c4d3111529149c042feeb39bed45b655e8f589
Pi Thanacha Choopojcharoen <tchoopojcharoen@gmail.com>
2022-08-26 04:03:08 +0700

    Add files via upload

---
+++ install_pkg.bash
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+
+replace_pkg_name () {
+    if [[ package_name != "" && $2 != "" ]]; then
+    sed -i "s/package_name/$2/" $1
+    fi
+}
+
+cp -r ROS2_package_cpp_python/package_name ~/$1/src
+rm -rf ROS2_package_cpp_python
+cd ~
+cd $1/src/
+ls
+
+
+mv package_name $2
+
+mv $2/package_name $2/$2
+mv $2/include/package_name $2/include/$2
+
+replace_pkg_name $2/package.xml $2
+replace_pkg_name $2/CMakeLists.txt $2
+replace_pkg_name $2/src/cpp_node.cpp $2
+replace_pkg_name $2/scripts/dummy_script.py $2
+replace_pkg_name $2/$2/dummy_module.py $2
+
+cd ~
+cd $1
+colcon build --packages-select $2
+source install/setup.bash
+
